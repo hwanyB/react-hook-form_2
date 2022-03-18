e01ff942190329d6de88e6f8e6c386ff6984eb6c
hwanyb <mnn9502@naver.com>
2022-03-18 11:00:00 +0900

    Create project

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+require('@babel/polyfill')
+
+
+module.exports = {
+    entry: {
+      app: [
+          '@babel/polyfill',
+          path.join(__dirname, "main.js")
+      ],
+    },
+    output: {
+      filename: "[name].js",
+      path: path.join(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+        {
+          test:/\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+              'vue-style-loader',
+              'css-loader'
+          ]
+        }
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+          template: path.join(__dirname, 'index.html')
+      }),
+      new VueLoaderPlugin(),
+    ],
+  };
+  
