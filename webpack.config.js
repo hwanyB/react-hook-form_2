bace8a132c1b583268551ef556a0288faadfbd86
hwanyb <mnn9502@naver.com>
2022-03-18 11:00:00 +0900

    Update devtool for webpack production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const { VueLoaderPlugin } = require('vue-loader')
-require('@babel/polyfill')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+const merge = require("webpack-merge");
+require("@babel/polyfill");
 
 
-module.exports = {
+module.exports = (env, opts) => {
+  const config = {
+    // 개발용, 제품용 중복되는 옵션들..
     entry: {
-      app: [
-          '@babel/polyfill',
-          path.join(__dirname, "main.js")
-      ],
+      app: ["@babel/polyfill", path.join(__dirname, "main.js")],
     },
     output: {
       filename: "[name].js",
@@ -24,31 +24,42 @@ module.exports = {
           loader: "vue-loader",
         },
         {
-          test:/\.js$/,
+          test: /\.js$/,
           exclude: /node_modules/,
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
         {
           test: /\.css$/,
-          use: [
-              'vue-style-loader',
-              'css-loader'
-          ]
-        }
+          use: ["vue-style-loader", "css-loader"],
+        },
       ],
     },
     plugins: [
       new HtmlWebpackPlugin({
-          template: path.join(__dirname, 'index.html')
+        template: path.join(__dirname, "index.html"),
       }),
       new VueLoaderPlugin(),
       new CopyPlugin([
         {
-          from: 'assets/',
-          to: ''
-        }
+          from: "assets/",
+          to: "",
+        },
       ]),
-      new CleanWebpackPlugin()
     ],
   };
-  
+  if (opts.mode === "development") {
+    return merge(config, {
+      // 개발용 옵션 추가
+      devtool: "eval", // 빌드시간이 적게 들고 디버깅 가능, 최적화가 덜 되어있고 용량이 큼
+      devServer: {
+        open: false,
+      },
+    });
+  } else {
+    return merge(config, {
+      //제품용 옵션 추가
+      devtool: "cheap-module-source-map", // 최적화 되어있고 용량이 작음, 디버깅불가능하고 빌드시간 오래걸림
+      plugins: [new CleanWebpackPlugin()], // dist 내 파일 갱신
+    });
+  }
+};
