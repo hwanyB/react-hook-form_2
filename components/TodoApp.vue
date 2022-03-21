07e74db44bb35f3a865148ca3cf90e4321022790
hwanyb <mnn9502@naver.com>
2022-03-21 11:00:00 +0900

    CRUD - Create Todo

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -5,6 +5,10 @@
     </div>
 </template>
 <script>
+import lowdb from 'lowdb'
+import LocalStorage from 'lowdb/adapters/LocalStorage'
+import cryptoRandomString from 'crypto-random-string'
+
 import TodoItem from "./TodoItem";
 import TodoCreator from "./TodoCreator";
 
@@ -12,6 +16,34 @@ export default {
     components: {
         TodoCreator,
         TodoItem
+    },
+    data () {
+        return {
+            db: null
+        }
+    },
+    created (){
+        this.initDB()
+    },
+    methods: {
+        initDB () {
+            const adapter = new LocalStorage('todo-app')
+            this.db = lowdb(adapter)
+
+            this.db.defaults({
+                todos: []
+            }).write()
+        },
+        createTodo (title) {
+            const newTodo = {
+                id: cryptoRandomString({ length: 10 }),
+                title,
+                createAt: new Date(),
+                updatedAt: new Date(),
+                done: false,
+            }
+            this.db.get('todos').push(newTodo).write()
+        }
     }
 }
 </script>
