0bc156867c7a1fb773eb21c38f5808946937d3bf
hwanyb <mnn9502@naver.com>
2022-03-24 11:00:00 +0900

    Add Todo filters

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>
-        <todo-item v-for="todo in todos" :key="todo.id" :todo="todo" @update-todo="updateTodo"
-            @delete-todo="deleteTodo" />
+    <div class="todo-app">
+        <div class="todo-app__actions">
+            <div class="filters">
+                <button :class="{ active: filter === 'all'}" @click="changeFilter('all')">모든 항목 ({{ total }})</button>
+                <button :class="{ active: filter === 'active'}" @click="changeFilter('active')">해야 할 항목 ({{ activeCount }})</button>
+                <button :class="{ active: filter === 'completed'}" @click="changeFilter('completed')">완료된 항목 ({{ completedCount }})</button>
+            </div>
+        </div>
         <hr />
-        <todo-creator @create-todo="createTodo" />
+        <div class="todo-app__list">
+            <todo-item v-for="todo in filteredTodos" :key="todo.id" :todo="todo" @update-todo="updateTodo"
+                @delete-todo="deleteTodo" />
+        </div>
+        <hr />
+        <todo-creator class="todo-app__creator" @create-todo="createTodo" />
     </div>
 </template>
 <script>
@@ -26,7 +36,29 @@ export default {
     data() {
         return {
             db: null,
-            todos: []
+            todos: [],
+            filter: 'all'
+        }
+    },
+    computed: {
+        filteredTodos() {
+            switch (this.filter) {
+                case 'all': default:
+                    return this.todos
+                case 'active':
+                    return this.todos.filter(todo => !todo.done)
+                case 'completed':
+                    return this.todos.filter(todo => todo.done)
+            }
+        },
+        total (){
+            return this.todos.length
+        },
+        activeCount (){
+            return this.todos.filter(todo => !todo.done).length
+        },
+        completedCount (){
+            return this.total - this.activeCount
         }
     },
     created() {
@@ -45,8 +77,6 @@ export default {
                     todos: []
                 }).write()
             }
-
-
         },
         createTodo(title) {
             const newTodo = {
@@ -68,7 +98,17 @@ export default {
             this.db.get('todos').remove({ id: todo.id }).write()
             const foundIndex = _findIndex(this.todos, { id: todo.id })
             this.$delete(this.todos, foundIndex)
+        },
+        changeFilter(filter){
+            this.filter = filter
         }
     }
 }
 </script>
+<style scoped lang="scss">
+    button.active{
+        font-weight: bold;
+        background-color: darkblue;
+        color: #fff;
+    }
+</style>></style>
