1b7e05e4fd80854f87a68d4438a503ea4eaeb8b2
hwanyb <mnn9502@naver.com>
2022-03-24 11:00:00 +0900

    TodoItem component - Edit data

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,60 +1,88 @@
 <template>
     <div id="todo-item">
-        <input type="checkbox" v-model="done" />
-        <div class="item__title-wrap"></div>
+        <div class="item__inner" item--edit v-if="isEditMode">
+            <input autofocus="true" type="text" :value="editedTitle" @input="editedTitle = $event.target.value"
+                @keypress.enter="editedTodo" @keypress.esc="offEditMode" />
+            <div class="item__actions">
+                <button key="complete" @click="editedTodo">완료</button>
+                <button key="cancle" @click="offEditMode">취소</button>
+            </div>
+        </div>
+        <div class="item__inner" item--normal v-else>
+            <input type="checkbox" v-model="done" />
+            <div class="item__title-wrap"></div>
             <div class="item__title">
                 {{ todo.title }}
             </div>
             <div class="item__date">
                 {{ computedDate }}
             </div>
-        <div class="item__actions">
-            <button @click="onEditMode">수정</button>
-            <button @click="delelteTodo">삭제</button>
+            <div class="item__actions">
+                <button key="update" @click="onEditMode">수정</button>
+                <button key="delete" @click="delelteTodo">삭제</button>
+            </div>
         </div>
+
     </div>
 </template>
 <script>
-    import dayjs from 'dayjs'
-    export default {
-        props: {
-            todo: Object
+import dayjs from 'dayjs'
+export default {
+    props: {
+        todo: Object
+    },
+    data() {
+        return {
+            isEditMode: false,
+            editedTitle: ''
+        }
+    },
+    methods: {
+        editedTodo() {
+            if(this.todo.title !== this.editedTitle){
+                this.updateTodo({
+                title: this.editedTitle,
+                updatedAt: new Date()
+            })
+            }
+            this.offEditMode()
         },
-        methods: {
-            onEditMode () {
-
-            },
-            offEditMode () {
-
-            },
-            updateTodo (value){
-                this.$emit('update-todo', this.todo, value)
-            },
-            delelteTodo () {
-                this.$emit('delete-todo', this.todo)
-            },
+        onEditMode() {
+            this.isEditMode = true
+            this.editedTitle = this.todo.title
         },
-        computed: {
-            done: {
-                get(){
-                    return this.todo.done
-                },
-                set(done){
-                    this.updateTodo({
-                        done
-                    })
-                }
-            },
-            computedDate () {
-                const date = dayjs(this.todo.createdAt)
-                const isSame = date.isSame(this.todo.updatedAt)
+        offEditMode() {
+            this.isEditMode = false
 
-                if (isSame){
-                    return date.format('YY년 M월 D일')
-                } else {
-                    return `${date.format('YY년 M월 D일')} (edited)`
-                }
+        },
+        updateTodo(value) {
+            this.$emit('update-todo', this.todo, value)
+        },
+        delelteTodo() {
+            this.$emit('delete-todo', this.todo)
+        },
+    },
+    computed: {
+        done: {
+            get() {
+                return this.todo.done
+            },
+            set(done) {
+                this.updateTodo({
+                    done
+                })
+            }
+        },
+        computedDate() {
+            const date = dayjs(this.todo.createdAt)
+            const isSame = date.isSame(this.todo.updatedAt)
+
+            if (isSame) {
+                return date.format('YY년 M월 D일')
+            } else {
+                return `${date.format('YY년 M월 D일')} (edited)`
             }
         }
     }
+}
 </script>
