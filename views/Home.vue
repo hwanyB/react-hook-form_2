5f18223a8f60df9254b7b281164780b933862951
hwanyb <mnn9502@naver.com>
2022-03-26 11:00:00 +0900

    Router-link

---
+++ views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="home">
+        <div>
+            <h1>HOME</h1>
+            <router-link to="/todos">TODO APP</router-link>
+        </div>
+    </section>
+</template>
+<script>
+</script>
